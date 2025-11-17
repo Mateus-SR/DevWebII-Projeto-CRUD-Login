4848f9304591901ab971edafd3daac5d12d46f9a
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-11-16 22:58:25 -0300

    Corrigindo typo

--- loginCRUD/models/usuario.js
+++ loginCRUD/models/usuario.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const usuarioSchema = new Schema({
@@ -10,13 +10,13 @@ const usuarioSchema = new Schema({
 
 usuarioSchema.pre('save', async function(next) {
     if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
+        this.password = await bcryptjs.hash(this.password, 10);
     }
     next();
 });
 
 usuarioSchema.methods.comparePassword = function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+    return bcryptjs.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
