1a024ff671e8cc600b3e8999e52fe59b27858788
Mateus Santos Rodrigues <hiroshi.reserva@gmail.com>
2025-11-19 10:45:21 -0300

    Arrumando falta de import de js

--- loginCRUD/public/javascripts/header.js
+++ loginCRUD/public/javascripts/header.js
@@ -1,10 +1,13 @@
+import { getTokenJWT, setTokenJWT, logout, checkAuth } from './config';
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const botaoConta = document?.getElementById('botaoConta'); // Seleciona o span da engrenagem
     
     if (botaoConta) {
-        const token = getTokenJWT();
+        const jwt_token = getTokenJWT();
 
-        if (token) {
+        if (jwt_token) {
             botaoConta.textContent = "Sair";
             botaoConta.onclick = () => {
                 logout(); // Chama a função do config.js
